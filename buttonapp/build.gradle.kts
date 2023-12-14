3bcc7b3b4db1a3e88f26f7069bbffc77d220529e
noerojasf <146777762+noerojasf@users.noreply.github.com>
2023-12-14 11:36:20 -0600

    Update release 2.0.1
    Add jitpack.yml

--- buttonapp/build.gradle.kts
+++ buttonapp/build.gradle.kts
@@ -71,7 +71,7 @@ publishing{
         register<MavenPublication>("release") {
             groupId = "com.github.non1692"
             artifactId = "CustomButton"
-            version = "2.0.0"
+            version = "2.0.1"
 
             afterEvaluate {
                 from(components["release"])
